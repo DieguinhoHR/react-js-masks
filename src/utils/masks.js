8049acb5093cde18d2bd498dca22bad62ba8ade9
Isabela Aquino <isabela.isavzp@gmail.com>
2020-07-18 20:57:20 -0300

    add mask de RG e real brasileiro (R$)

--- src/utils/masks.js
+++ src/utils/masks.js
@@ -44,10 +44,36 @@ function pisMask (value) {
     .replace(/(-\d{1})\d+?$/, '$1')
 }
 
+
+function identidadeMask (value) {
+  return value
+  .replace(/\D/g, '')
+  .replace(/(\d{2})(\d)/, '$1.$2')
+  .replace(/(\d{3})(\d)/, '$1.$2')
+  .replace(/(\d{3})(\d)/, '$1-$2')
+  .replace(/(-\d{1})\d+?$/, '$1')
+}
+
+function realMoedaMask (value) {
+  String(value)
+  
+      value = value 
+          .replace(/\D/g, '')
+          .replace(/([0-9]{2})$/g, ",$1")
+      
+      if( value.length > 6 )
+         value = value.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2")
+
+      return value
+ }
+
 export { 
   cpfMask, 
   cnpjMask, 
   phoneMask, 
   cepMask, 
-  pisMask
+  pisMask,
+  identidadeMask,
+  realMoedaMask
 }
+
